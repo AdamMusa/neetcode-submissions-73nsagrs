73270c884cf1de10fc4934c4c8d1a7e67d8ddbff
AdamMusa <adammusa2222@gmail.com>
2026-04-06 09:34:27 -0400

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows,cols = len(grid), len(grid[0])
+
+        def backtrack(r,c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
+                return 0
+            
+            grid[r][c] = 0
+
+            found = (
+                backtrack(r+1, c) + 
+                backtrack(r -1,c) + 
+                backtrack(r, c +1) + 
+                backtrack(r, c - 1)
+            )
+
+            return 1 + found
+        area = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    area = max(area, backtrack(r,c))
+        return area
